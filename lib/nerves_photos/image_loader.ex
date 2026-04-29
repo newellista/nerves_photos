b5fa364aca39637d34a9d6d3bbe238b4d18ddc8c
Steve Newell <steve.newell@lvt.com>
2026-04-29 16:07:02 -0600

    feat: add ImageLoader GenServer using Immich preview endpoint

---
+++ lib/nerves_photos/image_loader.ex
@@ -0,0 +1,68 @@
+defmodule NervesPhotos.ImageLoader do
+  use GenServer
+  require Logger
+
+  @stream_key "photo:current"
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  def load(asset_id, reply_to \\ nil, stream_key \\ @stream_key) do
+    GenServer.cast(__MODULE__, {:load, asset_id, reply_to || self(), stream_key})
+  end
+
+  @impl true
+  def init(opts) do
+    {:ok,
+     %{
+       url: opts[:url] || Application.fetch_env!(:nerves_photos, :immich_url),
+       api_key: opts[:api_key] || Application.fetch_env!(:nerves_photos, :immich_api_key),
+       req_options: opts[:req_options] || [],
+       put_fn: opts[:put_fn] || &stream_put/2
+     }}
+  end
+
+  @impl true
+  def handle_cast({:load, asset_id, reply_to, stream_key}, state) do
+    Task.start(fn ->
+      case fetch_preview(asset_id, state) do
+        {:ok, image_bytes} ->
+          case state.put_fn.(stream_key, image_bytes) do
+            :ok ->
+              send(reply_to, {:image_loaded, stream_key})
+
+            {:error, reason} ->
+              Logger.warning("ImageLoader: stream put failed for #{asset_id}: #{inspect(reason)}")
+              send(reply_to, {:image_load_error, asset_id})
+          end
+
+        {:error, reason} ->
+          Logger.warning("ImageLoader: failed to load #{asset_id}: #{inspect(reason)}")
+          send(reply_to, {:image_load_error, asset_id})
+      end
+    end)
+
+    {:noreply, state}
+  end
+
+  defp fetch_preview(asset_id, state) do
+    req =
+      Req.new(
+        [base_url: state.url, headers: [{"x-api-key", state.api_key}]] ++ state.req_options
+      )
+
+    case Req.get(req, url: "/api/assets/#{asset_id}/thumbnail", params: [size: "preview"]) do
+      {:ok, %{status: 200, body: body}} when is_binary(body) -> {:ok, body}
+      {:ok, %{status: status}} -> {:error, {:http, status}}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  defp stream_put(stream_key, image_bytes) do
+    case Scenic.Assets.Stream.Image.from_binary(image_bytes) do
+      {:ok, img} -> Scenic.Assets.Stream.put(stream_key, img)
+      {:error, :invalid} -> {:error, :invalid_image}
+    end
+  end
+end
---
+++ test/nerves_photos/image_loader_test.exs
@@ -0,0 +1,48 @@
+defmodule NervesPhotos.ImageLoaderTest do
+  use ExUnit.Case, async: true
+
+  alias NervesPhotos.ImageLoader
+
+  @fake_jpeg <<0xFF, 0xD8, 0xFF, 0xE0, 0, 0, 0>>
+
+  setup do
+    test_pid = self()
+
+    Req.Test.stub(ImageLoader, fn conn ->
+      if String.contains?(conn.request_path, "/thumbnail") do
+        conn
+        |> Plug.Conn.put_resp_header("content-type", "image/jpeg")
+        |> Plug.Conn.send_resp(200, @fake_jpeg)
+      else
+        Req.Test.json(conn, %{"error" => "not found"})
+      end
+    end)
+
+    Req.Test.allow(ImageLoader, test_pid, fn -> GenServer.whereis(ImageLoader) end)
+
+    {:ok, _pid} =
+      start_supervised(
+        {ImageLoader,
+         url: "http://immich.test",
+         api_key: "test-key",
+         req_options: [plug: {Req.Test, ImageLoader}],
+         put_fn: fn _key, _bytes -> :ok end}
+      )
+
+    :ok
+  end
+
+  test "load/2 sends {:image_loaded, key} to caller on success" do
+    ImageLoader.load("asset-1", self())
+    assert_receive {:image_loaded, "photo:current"}, 500
+  end
+
+  test "load/2 sends {:image_load_error, asset_id} on HTTP failure" do
+    Req.Test.stub(ImageLoader, fn conn ->
+      Plug.Conn.send_resp(conn, 404, "not found")
+    end)
+
+    ImageLoader.load("bad-asset", self())
+    assert_receive {:image_load_error, "bad-asset"}, 500
+  end
+end
