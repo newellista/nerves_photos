e33521928fb7397c4e74e0a58e7b7f3b256e807f
Steve Newell <steve.newell@lvt.com>
2026-04-30 14:36:18 -0600

    fix: correct Scenic viewport drivers config (keyword list → list of maps)

--- config/config.exs
+++ config/config.exs
@@ -23,8 +23,7 @@ config :nerves_photos, :viewport, %{
   size: {1920, 1080},
   default_scene: NervesPhotos.Scene.Main,
   drivers: [
-    module: Scenic.Driver.Local,
-    opts: [device: "/dev/fb0"]
+    %{module: Scenic.Driver.Local, opts: []}
   ]
 }
 
