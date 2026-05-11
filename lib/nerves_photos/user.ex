52406c85273bcc0d2db4b3bb3d1ca69fed9815c7
Steve Newell <steve.newell@lvt.com>
2026-05-11 13:03:22 -0600

    feat: add NervesPhotos.User with bcrypt hashing and validation

---
+++ lib/nerves_photos/user.ex
@@ -0,0 +1,31 @@
+defmodule NervesPhotos.User do
+  @moduledoc false
+
+  @valid_roles ~w(admin editor)
+
+  def new(username, password, role) do
+    with :ok <- validate_username(username),
+         :ok <- validate_password(password),
+         :ok <- validate_role(role) do
+      {:ok,
+       %{
+         username: username,
+         password_hash: Bcrypt.hash_pwd_salt(password),
+         role: role
+       }}
+    end
+  end
+
+  def verify_password(%{password_hash: hash}, password) do
+    Bcrypt.verify_pass(password, hash)
+  end
+
+  defp validate_username(u) when is_binary(u) and u != "", do: :ok
+  defp validate_username(_), do: {:error, "username cannot be blank"}
+
+  defp validate_password(p) when is_binary(p) and byte_size(p) >= 8, do: :ok
+  defp validate_password(_), do: {:error, "password must be at least 8 characters"}
+
+  defp validate_role(r) when r in @valid_roles, do: :ok
+  defp validate_role(_), do: {:error, "role must be admin or editor"}
+end
